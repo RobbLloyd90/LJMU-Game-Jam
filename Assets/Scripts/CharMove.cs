11c0ff521a05d931fda90331501a35254ca98a3e
RobbLloyd90 <robbmorgan10@gmail.com>
2015-06-24 22:34:05 +0100

    Initial Commit

---
+++ Assets/Scripts/CharMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+
+			Debug.Log ("penguin moves");
+			transform.position += Vector3.forward * (Time.deltaTime) * 20;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			
+			Debug.Log ("penguin rotating");
+			transform.rotation = Quaternion.AngleAxis  (30, Vector3.up);
+		}
+	}
+}
